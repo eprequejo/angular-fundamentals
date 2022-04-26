ac13633eda3e49d65a9485c530fb7ef9b78bf67b
eprequejo <erequejo@companywatch.net>
2022-04-26 08:28:38 +0100

    Add onInit

--- src/app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
+++ src/app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { Passenger } from "../../models/passenger.interface";
 
 
@@ -7,13 +7,19 @@ import { Passenger } from "../../models/passenger.interface";
     styleUrls: ["./passenger-dashboard.component.css"],
     templateUrl: "./passenger-dashboard.component.html"
 })
-export class PassengerDashboardComponent {
+export class PassengerDashboardComponent implements OnInit {
 
-    passengers: Array<Passenger> = [
-        { id: 1, name: "estela", checked: true, date: 1490742000000, children: [{ name: "leo", age: 8 }, { name: "eric", age: 4 }]},
-        { id: 2, name: "felipe", checked: false},
-        { id: 3, name: "leo", checked: true, date: 1490742000000},
-        { id: 4, name: "eric", checked: true, date: 1490742000000}
-      ];
+    passengers: Array<Passenger> = [];
+
+    ngOnInit(): void {
+
+        this.passengers = [
+            { id: 1, name: "estela", checked: true, date: 1490742000000, children: [{ name: "leo", age: 8 }, { name: "eric", age: 4 }]},
+            { id: 2, name: "felipe", checked: false},
+            { id: 3, name: "leo", checked: true, date: 1490742000000},
+            { id: 4, name: "eric", checked: true, date: 1490742000000}
+        ];
+        
+    }
 
 }
